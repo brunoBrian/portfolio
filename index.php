6a869f270057b805661db69ed90a8828692b1d6a
Bruno <bruninho_brian@hotmail.com>
2017-07-26 02:14:34 -0300

    initializing project

---
+++ index.php
@@ -0,0 +1,122 @@
+
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=8">
+      <meta http-equiv="X-UA-Compatible" content="IE=9" />
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+     
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="icon" href="../../favicon.ico">
+
+      <title>Itelios</title>
+
+      
+      <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="assets/fonts/stylesheet.css" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet">
+
+      <!--[if lt IE 10]>
+       <link href="css/ie.css" rel="stylesheet">
+      <![endif]-->
+
+    </head>
+
+    <body>
+
+      <!-- cabeçalho -->
+
+      <div class="container">
+        <div class="header">
+          <h1><img src="imagens/titulo.png"></h1>
+        </div>
+
+      <!-- Share esquerda -->
+
+        <div class="content-main col-md-12">
+          <div class="col-md-1 esquerda">
+            <div class="data-post">
+              <span class="dia">05</span>
+              <span class="mes">jul</span>
+              <span class="ano">2012</span>
+            </div>
+            <div class="socials">
+              <span class="btn-like"></span>
+              <span class="btn-tweet"></span>
+            </div>
+          </div>
+
+      <!-- Centro -->
+
+          <div class="content col-md-8"> 
+            <h3 class="title">Itelios cria novo teste de HTML!</h3>
+            <p class="author"><span class="menor">postado por</span> <span class="nome">Itelios Brasil</span></p>
+            <div class="post-content">
+                <p>Cupcake ipsum dolor sit. Amet gummies croissant jelly-o wafer croissant chocolate cake. Chocolate cake donut caramels croissant marzipan. Dragée topping danish jujubes cotton candy. Cookie cake icing wypas macaroon tootsie roll jelly bonbon bear claw. Cotton candy applicake marshmallow sweet roll tiramisul cheesecake ice cream applicake gummi bears.</p>
+                <p>Jelly marzipan souffé wypas. Jelly beans bear claw brownie bear claw. sweet tiramisu wypas. Carrot cake apple pie marzipan. Chocolate cakejelly beans sweet roll carrot cake cookie souffé caramels marshmallow. Cheesecake icing toffee dragée. Jelly beans powder tootsie roll. Sweet roll topping cookie chocolate cake cookie marshmallow topping faworki chocolate. Apple pie powder bear claw jelly sweet pastry cheesecake oat cake. Donut bear claw tootsie roll tiramisu.</p>
+                <p>Jelly marzipan souffé wypas. Jelly beans bear claw brownie bear claw. sweet tiramisu wypas. Carrot cake apple pie marzipan. Chocolate cakejelly beans sweet roll carrot cake cookie souffé caramels marshmallow. Cheesecake icing toffee dragée. Jelly beans powder tootsie roll. Sweet roll topping cookie chocolate cake cookie marshmallow topping faworki chocolate. Apple pie powder bear claw jelly sweet pastry cheesecake oat cake. Donut bear claw tootsie roll tiramisu.</p>
+                <p>Jelly marzipan souffé wypas. Jelly beans bear claw brownie bear claw. sweet tiramisu wypas. Carrot cake apple pie marzipan. Chocolate cakejelly beans sweet roll carrot cake cookie souffé caramels marshmallow. Cheesecake icing toffee dragée. Jelly beans powder tootsie roll. Sweet roll topping cookie chocolate cake cookie marshmallow topping faworki chocolate. Apple pie powder bear claw jelly sweet pastry.</p>
+            </div>
+            <div class="botoes">
+              <span class="btn-teste"><label>botão de teste</label></span>
+              <span class="btn-amigo"><label>enviar a um amigo</label></span>
+              <span class="btn-imprimir"><label>imprimir</label></span>
+            </div>
+
+        <!--Centro / Comentários -->
+
+            <div class="meio">
+              <h4>Comentários<h4>
+            </div>
+
+            <div class="comentarios">
+              <ul>
+                <li><span class="btn-profile"></span></li>
+                <li><p id="usuario1">Maria da Silva</p></li>
+                <li><p class="descricao">Jelly marzipan souffé wypas. Jelly beans bear claw brownie bear claw. sweet tiramisu wypas. Carrot cake apple pie marzipan. Chocolate cakejelly beans sweet roll carrot cake cookie souffé caramels marshmallow.</p></li>
+              </ul>
+              <hr>
+              <ul>
+                <li><span class="btn-profile1"></span></li>
+                <li><p id="usuario2">José Carlos Sousa</p></li>
+                <li><p class="descricao">Jelly marzipan souffé wypas. Jelly beans bear claw brownie bear claw. sweet tiramisu wypas. Carrot cake apple pie marzipan. Chocolate cakejelly beans sweet roll carrot cake cookie souffé caramels marshmallow Chocolate cake jelly beans sweet roll carrot cake cookie soufée Jelly marzipan soufée wypas. Jelly beans bear claw brownie bear claw.</p></li>
+              </ul>
+              <hr>
+              <ul>
+                <li><span class="btn-profile2"></span></li>
+                <li><p id="usuario3">Maria da Silva</p></li>
+                <li><p class="descricao">Jelly marzipan souffé wypas. Jelly beans bear claw brownie bear claw. sweet tiramisu wypas. Carrot cake apple pie marzipan. Chocolate cakejelly beans sweet roll carrot cake cookie souffé caramels marshmallow.</p></li>
+              </ul>
+              <hr>
+            </div>
+
+        <!-- Formulário -->
+
+            <form>
+              <ul class="form">
+                <li id="nome-form">Nome ou apelido</li>
+                <li class="mail">E-mail(eu@exemplo.com)</li><br>
+              </ul>
+              <input  class="name"type="text"></li>
+              <input  class="email" type="text"></li>
+              <ul>
+               <li id="comentario">Comentário</li>
+              </ul>
+              <textarea class="coment" type="text"></textarea>
+            </form>
+            <span class="btn-enviar"><label>enviar</label></span>
+        </div>
+          </div>
+        </div>
+      </div>
+
+    <!-- Rodapé  -->
+
+
+      <div class="footer col-md-12">
+        <p> <img src="imagens/rodape.png"> </p>
+      </div>
+    </body>
+  </html>
